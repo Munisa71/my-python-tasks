3ea1e8aa1eb84cbcc9bed02c5035f262fc3debf5
Munisa71 <rmunisa1111@gmail.com>
2026-04-07 15:56:21 +0300

    Update sort_numbers.py

--- sort_numbers.py
+++ sort_numbers.py
@@ -5,4 +5,4 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 num = [10, 5, 2, 8, 3, 100, 45, 1]
 num.sort()
 print("Отсортированный список:", num)
-print("Колчество чисел в списке:", len())
+print("Колчество чисел в списке:", len(num))
